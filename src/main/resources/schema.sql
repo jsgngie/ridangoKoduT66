2e0e38c9def5214931ba7a7086fdf46149ed02eb
Joosep Suuder <115637478+jsgngie@users.noreply.github.com>
2024-09-17 20:52:26 +0300

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,22 @@
 CREATE TABLE IF NOT EXISTS highscores(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    score BIGINT NULL
+    name varchar(256),
+    score BIGINT
+);
+
+CREATE TABLE IF NOT EXISTS drinks(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    drinkId BIGINT,
+    strDrink varchar(256),
+    strGlass varchar(256),
+    strInstructions varchar(1000),
+    strDrinkThumb varchar(256)
+);
+
+CREATE TABLE IF NOT EXISTS ingredients(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    drinkId BIGINT,
+    name varchar(256),
+    measure varchar(256),
+    FOREIGN KEY (drinkId) REFERENCES drinks(Id)
 );
